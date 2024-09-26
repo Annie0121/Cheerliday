cd9e050b9876b135ced1975481378b51c4b5555e
annie <rabbit110236@gmail.com>
2024-09-26 16:35:51 +0800

    add Drag function

--- app/[userid]/trip/[recordid]/components/Schedule.tsx
+++ app/[userid]/trip/[recordid]/components/Schedule.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState ,useRef} from 'react';
+import React, { useEffect, useState ,useRef,} from 'react';
 import{getDurationTime} from '../utils/getDurationTime'
 import { db, auth } from '@/app/firebase';
 import { doc, updateDoc } from "firebase/firestore"; 
@@ -9,7 +9,8 @@ import {TimeComponent} from '../components/TimeComponent'
 import{ DurationComponent  } from '../components/DurationComponent';
 import carImage from '../car.png'
 import style from "../styles/schedule.module.css"
-
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { sources } from 'next/dist/compiled/webpack/webpack';
 
 
 const colors=['#d05b6eff ',"#45818eff","#c1683cff","#a64d79ff","#a28c37ff","#8075b5ff","#6aa84fff"]
@@ -64,6 +65,8 @@ export function Schedule({record,setSelectedDay,travelTimes,setRecord,}:Schedule
     const [AttractionIndex, setAttractionIndex] = useState<number| null>(null);
     const [selectedDateIndex, setSelectedDateIndex] = useState<number| null>(null);
     const [calculatedTimes, setCalculatedTimes] = useState<{ [key: string]: { startTime: string, endTime: string }[] }>({});
+    const [AutoScrollEnabled,setAutoScrollEnabled]=useState(true)
+    
     //組件渲染後訪問和操作 DOM 元素
     const myRef = useRef<HTMLDivElement>(null);
     //點擊左右
@@ -110,7 +113,7 @@ export function Schedule({record,setSelectedDay,travelTimes,setRecord,}:Schedule
       }
     },[travelTimes])
 
-  
+      //計算景點開始結束的時間
         const calculateTimes = (date: DateRangeItem) => {
           if (Object.keys(travelTimes).length === 0) {
             return [];
@@ -135,24 +138,28 @@ export function Schedule({record,setSelectedDay,travelTimes,setRecord,}:Schedule
             return(times);
         
         };
-   
+
+
+    //獲取新增景點的日期
     function addplace(date:string){
         setSelectedDay(date);      
     }
     
     const dateRef = useRef<(HTMLDivElement | null)[]>([]);
 
+    //滑動到點選的日期
     const getdate =(index:number)=>{
-        const element = dateRef.current[index];
-        if(element){
-            element.scrollIntoView(
-                {behavior:'smooth'}
-            );
-        }
-        
-        
-    }
+      const element = dateRef.current[index];
+      if(element){
+          element.scrollIntoView(
+              {behavior:'smooth'}
+          );
+      }
+      
+      
+  }
 
+    //設定刪除邏輯
     const handleDel= async(attractionindex:number,dateindex:number)=>{
       
       const updatedDateRange = [...record.dateRange];
@@ -172,25 +179,73 @@ export function Schedule({record,setSelectedDay,travelTimes,setRecord,}:Schedule
         ...preRecord,
         dateRange:updatedDateRange
       }))
-
-
     }
 
+    //設定拖拉後的結果
+    const onDragEnd =async(event:any)=>{
+      const { source, destination } = event;
+     
+      
+      if(!destination)return;
+      //拷貝資料
+      const newDateRange = [...record.dateRange];
+      const sourceDateIndate = source.droppableId
+      const sourceAttractionIndex = source.index
+      const destinationDateIndex =destination.droppableId
+      const destinationAttractionIndex =destination.index
+
+      //處理拖曳後資料
+      const [remove] = newDateRange[sourceDateIndate].attractions.splice(sourceAttractionIndex,1)
+      newDateRange[destinationDateIndex].attractions.splice(destinationAttractionIndex,0,remove)
+     
+      
+
+      
+      //寫進數據庫
+      const url = window.location.href.split("/");
+        let recordId = url[5];
+        const recordRef = doc(db, "record", recordId);
+        try{
+          await updateDoc(recordRef, {
+            dateRange: newDateRange
+          });
+
+          setRecord((prevRecord: any) => ({
+            ...prevRecord,
+            dateRange: newDateRange
+          }));
+          setAutoScrollEnabled(true);
+        }
+        catch(error){
+          console.error( error);
+        }
+     
+      
+    }
+
+
+
+
+ 
     return(
         <div >
             <div style={{width:'100%'}}>
+               {/*行程大標題*/}
               <div className={style.schedule_info} style={{ backgroundImage: `url('/background/${record.backgroundImage}')` }}>
                 <div className={style.schedule_info_background} ></div>
                 <div className={style.schedule_info_name} >{name}</div>
                 <div className={style.schedule_info_date} >{startdate} ~ {enddate}</div>
               </div>
+                 {/*上標日期渲染*/}
                 <div style={{display:'flex',width:'100%',justifyContent:'space-between',height:'50px',boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)'}}>
                     <div style={{width:'40px',boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',fontSize:'30px',textAlign:'center',paddingTop:'5PX',color:'#ff4757',cursor:'pointer'}} onClick={listLeft}>&lt;</div>
-                    <div style={{ width: 'calc(100% - 80px)',display: 'flex',overflow: 'hidden'}} ref={myRef}>
+                    <div style={{ width: 'calc(100% - 80px)',display: 'flex',overflow: 'hidden'}} ref={myRef} >
                         {dateRange.map((date, index:number) => (
                         <div key={index} 
                             className={styles.date_bar}
-                            onClick={()=>getdate(index)}>
+                            onClick={()=>getdate(index)}
+                            id={`date-${index}`} 
+                            >
                             {date.date}
                             <div style={{color:'#666666ff',fontWeight:'400',fontSize:'14PX'}} >第{index+1}天</div>
                         </div>
@@ -201,11 +256,16 @@ export function Schedule({record,setSelectedDay,travelTimes,setRecord,}:Schedule
             </div>
 
             <div style={{backgroundColor:'#efefefff',paddingTop:'20PX',paddingBottom:'40px'}}> 
+
+             <DragDropContext onDragEnd={onDragEnd} onDragStart={()=>{setAutoScrollEnabled(false);}}>
                 {dateRange.map((date,dateindex)=>{
                    const times = calculateTimes(date);
                     return(
+
                     <React.Fragment key={dateindex} >
-                        <div style={{display:'flex',margin:'35px 10px'}}>
+                      
+                        <div style={{display:'flex',margin:'35px 10px 0 10px'}}>
+                             {/*行程的日期、出發時間等*/}
                             <div 
                                 style={{width:'65px',backgroundColor:colors[dateindex % colors.length],marginLeft:'10px',height:'30px',fontSize:'20px',padding:'5px',borderRadius:'6px',fontWeight:'600',color:'white', alignItems:'center',textAlign:'center'}}
                                 ref={el => {dateRef.current[dateindex] = el}}>  
@@ -224,70 +284,94 @@ export function Schedule({record,setSelectedDay,travelTimes,setRecord,}:Schedule
                               {date.date}
                             </p>
                         </div>
-                        
-                        {date.attractions && date.attractions.length > 0 && (
-                            date.attractions.map((attraction, attractionindex) => {
-                              
-                                return(
-                                  <React.Fragment key={attractionindex}>
-                                      <div  style={{width:'100%',boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 -4px 6px rgba(0, 0, 0, 0.1)',backgroundColor:'white',height:'130px', display:'flex',justifyContent:'center',alignItems:'center'}}>
-                                          <div style={{display:'flex',height:'80px',width:'95%'}}>
-                                            <img style={{height:'80px',width:'80px',overflow:'hidden'}} src={attraction.picture}></img>
-                                            <div style={{marginLeft:'10px',flexGrow: 1}}>
-                                              <div style={{display:'flex',height:'20px',width:'180px'}}>
-                                                  <div onClick={()=>{setAttractionIndex(attractionindex);setSelectedDateIndex(dateindex)}} style={{ alignSelf: 'flex-end',fontSize:'13px',textDecoration:'underline',fontWeight:'800',textUnderlineOffset: '3px',cursor:'pointer',color:'#7ab5d9ff'}}>
-                                                    {Math.floor(attraction.stayDuration / 60)} 小時 {attraction.stayDuration % 60} 分鐘
-                                                  </div>
+                         {/*該日期的景點名稱順序*/}
+                         <Droppable droppableId={dateindex.toString()}>
+                                {(provided, snapshot) => (
+                                  <div {...provided.droppableProps} ref={provided.innerRef} style={{margin:'35px 0'}}>
+                                      {date.attractions && date.attractions.length > 0 ? (
+                                        date.attractions.map((attraction, attractionindex) => {
 
-                                                  <div style={{fontSize:'13px',marginLeft:'5PX', alignSelf: 'flex-end',color:'#666666ff'}}>|</div>
+                                        return(
+                                              
+                                        <React.Fragment key={attractionindex}>
+                                          <Draggable  key={`${dateindex}-${attractionindex}`} draggableId={`${dateindex}-${attractionindex}`} index={attractionindex}>
+                                                {(provided)=>
+                                                (
+                                                  <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                  >
+                                                          <div  style={{width:'100%',boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 -4px 6px rgba(0, 0, 0, 0.1)',backgroundColor:'white',height:'130px', display:'flex',justifyContent:'center',alignItems:'center'}}>
+                                                              <div style={{display:'flex',height:'80px',width:'95%'}}>
+                                                                <img style={{height:'80px',width:'80px',overflow:'hidden'}} src={attraction.picture}></img>
+                                                                <div style={{marginLeft:'10px',flexGrow: 1}}>
+                                                                  <div style={{display:'flex',height:'20px',width:'180px'}}>
+                                                                      <div onClick={()=>{setAttractionIndex(attractionindex);setSelectedDateIndex(dateindex)}} style={{ alignSelf: 'flex-end',fontSize:'13px',textDecoration:'underline',fontWeight:'800',textUnderlineOffset: '3px',cursor:'pointer',color:'#7ab5d9ff'}}>
+                                                                        {Math.floor(attraction.stayDuration / 60)} 小時 {attraction.stayDuration % 60} 分鐘
+                                                                      </div>
 
-                                                  <div style={{fontSize:'13px',marginLeft:'5PX', alignSelf: 'flex-end',color:'#666666ff',fontWeight:'600'}}>
-                                                  {times && times[attractionindex] ? `${times[attractionindex].startTime} - ${times[attractionindex].endTime}` : ""}
+                                                                      <div style={{fontSize:'13px',marginLeft:'5PX', alignSelf: 'flex-end',color:'#666666ff'}}>|</div>
+
+                                                                      <div style={{fontSize:'13px',marginLeft:'5PX', alignSelf: 'flex-end',color:'#666666ff',fontWeight:'600'}}>
+                                                                      {times && times[attractionindex] ? `${times[attractionindex].startTime} - ${times[attractionindex].endTime}` : ""}
+                                                                      </div>
+                                                                  </div>
+                                                                  {
+                                                                    AttractionIndex !== null && selectedDateIndex !== null && 
+                                                                    (<DurationComponent  
+                                                                      selectedDateIndex={selectedDateIndex} 
+                                                                      dateRange={dateRange} 
+                                                                      setDateRange={setDateRange} 
+                                                                      AttractionIndex={AttractionIndex} 
+                                                                      setAttractionIndex={setAttractionIndex} 
+                                                                    />) 
+                                                                  }
+                                                                  <div style={{display:'flex',justifyContent:'space-between',width:'100%'}}>
+                                                                    <div style={{fontSize:'17PX',fontWeight:'900',marginTop:'8PX',}}>
+                                                                      {attraction.name}
+                                                                    </div>
+                                                                  </div>
+                                                                  <div style={{fontSize:'13px',marginTop:'3PX',color:'#6a6969ff',fontWeight:'500'}}>{attraction.address}</div>
+                                                                </div>
+                                                                <span style={{cursor:'pointer'}} onClick={()=>handleDel(attractionindex,dateindex)}>×</span>
+                                                              </div>  
+                                                          </div>
                                                   </div>
-                                              </div>
-                                              {
-                                                AttractionIndex !== null && selectedDateIndex !== null && 
-                                                (<DurationComponent  
-                                                  selectedDateIndex={selectedDateIndex} 
-                                                  dateRange={dateRange} 
-                                                  setDateRange={setDateRange} 
-                                                  AttractionIndex={AttractionIndex} 
-                                                  setAttractionIndex={setAttractionIndex} 
-                                                />) 
-                                              }
-                                              <div style={{display:'flex',justifyContent:'space-between',width:'100%'}}>
-                                                <div style={{fontSize:'17PX',fontWeight:'900',marginTop:'8PX',}}>
-                                                  {attraction.name}
+                                                )}
+                                            </Draggable>
+                                            {attractionindex < date.attractions.length - 1 && (
+                                                <div style={{borderLeft: '2px dashed #666666ff',height:'50px',marginLeft:'50px',display:'flex',alignItems:'center'  }}>
+                                                  {travelTimes[date.date] && travelTimes[date.date][attractionindex] && (
+                                                        <div style={{ margin: '10px',display:'flex' }}>
+                                                          <div style={{marginRight:'10px'}}>約 {travelTimes[date.date][attractionindex]} </div>
+                                                          <Image width={20} height={20} src={carImage} alt="Car" />
+                                                        </div>
+                                                    )}
                                                 </div>
-                                              </div>
-                                              <div style={{fontSize:'13px',marginTop:'3PX',color:'#6a6969ff',fontWeight:'500'}}>{attraction.address}</div>
-                                            </div>
-                                            <span style={{cursor:'pointer'}} onClick={()=>handleDel(attractionindex,dateindex)}>×</span>
-                                          </div>  
-                                      </div>
-                                      {attractionindex < date.attractions.length - 1 && (
-                                          <div style={{borderLeft: '2px dashed #666666ff',height:'50px',marginLeft:'50px',display:'flex',alignItems:'center'  }}>
-                                            {travelTimes[date.date] && travelTimes[date.date][attractionindex] && (
-                                                  <div style={{ margin: '10px',display:'flex' }}>
-                                                    <div style={{marginRight:'10px'}}>約 {travelTimes[date.date][attractionindex]} </div>
-                                                    <Image width={20} height={20} src={carImage} alt="Car" />
-                                                  </div>
-                                              )}
-                                          </div>
-                                      )}
-                                  </React.Fragment>
-                                  )
-                            })  
-                        )}
-                        <div style={{margin:'30px 100px',width:'70px',height:'80px',display: 'grid', placeItems: 'center'}}>
+                                            )}
+                                        </React.Fragment>
+                                            
+                                      )
+                                        })  
+                                    ):(<div style={{width:"100%" }}></div>)
+                                    }
+                                    {provided.placeholder}
+                                  </div>
+                                )}
+                              </Droppable>
+                        
+                        <div style={{margin:'0 100px 30px 100px',width:'70px',height:'80px',display: 'grid', placeItems: 'center'}}>
                             <div style={{width: '50px',height: '50px',lineHeight:'50px',boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',borderRadius:' 50%',display: 'flex',alignItems: 'center',justifyContent: 'center',backgroundColor: 'white',cursor:'pointer',fontSize:'35px'}}
                                  onClick={() => addplace(date.date)}>
                                     ＋
                             </div> 
                           <div style={{marginTop:'10PX',fontWeight:'600',color:'#666666ff'}}>加入景點</div>
                         </div>
+                      
                     </React.Fragment>)
                 })} 
+              </DragDropContext>
             </div>
         </div>
     )
